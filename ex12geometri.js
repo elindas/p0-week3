2fa708fbdede97b3e9e3f444a69918dec00942b4
Elinda Sintaresmi <elinda.sinta@gmail.com>
2019-08-21 13:11:25 +0700

    add

--- ex12geometri.js
+++ ex12geometri.js
@@ -1,20 +1,22 @@
-
 function tentukanDeretGeometri(arr) {
-    var hasilBagi = arr[1]/arr[0]; 
-    var result= true
+  var hasilBagi = arr[1] / arr[0];
+  var result;
 
-    for (i=2; i< arr.length; i++) {
-        if ((arr[i]/arr[i-1]) !== hasilBagi) {
-          result = false
-        } 
+  for (i = 2; i < arr.length; i++) {
+    if (arr[2] === undefined) {
+      result = undefined;
+    } else if (arr[i] / arr[i - 1] !== hasilBagi) {
+      result = false;
+    } else {
+      result = true
     }
-
-    return result;    
+  }
+  return result;
 }
-  
-  // TEST CASES
-  console.log(tentukanDeretGeometri([1, 3, 9, 27, 81])); // true
-  console.log(tentukanDeretGeometri([2, 4, 8, 16, 32])); // true
-  console.log(tentukanDeretGeometri([2, 4, 6, 8])); // false
-  console.log(tentukanDeretGeometri([2, 6, 18, 54])); // true
-  console.log(tentukanDeretGeometri([1, 2, 3, 4, 7, 9])); // false
+
+// TEST CASES
+console.log(tentukanDeretGeometri([1, 3, 9, 27, 81])); // true
+console.log(tentukanDeretGeometri([2, 4, 8, 16, 32])); // true
+console.log(tentukanDeretGeometri([2, 4, 6, 8])); // false
+console.log(tentukanDeretGeometri([2, 6, 18, 54])); // true
+console.log(tentukanDeretGeometri([1, 2, 3, 4, 7, 9])); // false
