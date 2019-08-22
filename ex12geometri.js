6ba26c7f6eb70e5273a28cbdfff1d421230be964
Elinda Sintaresmi <elinda.sinta@gmail.com>
2019-08-22 16:02:28 +0700

    add

--- ex12geometri.js
+++ ex12geometri.js
@@ -1,13 +1,19 @@
 function tentukanDeretGeometri(arr) {
   var hasilBagi = arr[1] / arr[0];
-  var result = false
+  var result;
 
   for (i = 2; i < arr.length; i++) {
-    if (arr[i] / arr[i - 1] === hasilBagi) {
-        result= true
+    if (arr[i] / arr[i - 1] !== hasilBagi) {
+      result = false;
+    } else {
+      result = true;
     }
   }
-  
+
+  if (result === undefined) {
+    return false;
+  }
+
   return result;
 }
 
@@ -17,4 +23,3 @@ console.log(tentukanDeretGeometri([2, 4, 8, 16, 32])); // true
 console.log(tentukanDeretGeometri([2, 4, 6, 8])); // false
 console.log(tentukanDeretGeometri([2, 6, 18, 54])); // true
 console.log(tentukanDeretGeometri([1, 2, 3, 4, 7, 9])); // false
-
